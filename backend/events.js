1168917d75c5fe1e7a977c0cbbf55f5a1e8defa8
frankline kebei <franklin.kebei@ibm.com>
2019-10-16 14:05:51 -0500

    updates

---
+++ backend/events.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+      id: 1,
+      title: 'Frankline Training',
+      detail: 'Monroe LA ',
+      date: '10-16-2019'
+    },
+    {
+      id: 2,
+      title: 'javaScript',
+      detail: 'Aprenticeship',
+      date: '06-10-2019'
+    },
+    {
+      id: 3,
+      title: 'Lunchpad Ends June 2020',
+      date: '06-10-2020'
+    }
+  ];
